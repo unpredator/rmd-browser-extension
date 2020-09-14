d90cff297af52393dfcafbde091e85819339715e
andy nam <naamoonoo.y@gmail.com>
2020-09-14 22:52:01 +0900

    feat: show title in reivew sidebar as example(#1)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-console.log("pull request page");
+const createElement = (tagName, attributes, ...childNodes) => {
+	const newElement = document.createElement(tagName);
+
+	for (const [key, value] of Object.entries(attributes)) {
+		// text
+		if (key === "text") {
+			const textNode = document.createTextNode(value);
+			newElement.appendChild(textNode);
+			continue;
+		}
+
+		// event
+		if (typeof value === "function") {
+			const eventName = key.slice(2);
+			if (!attributes.eventTarget) {
+				newElement.addEventListener(eventName, value);
+				continue;
+			}
+		}
+
+		newElement.setAttribute(key, value);
+	}
+
+	// childNodes
+	const fragment = document.createDocumentFragment();
+	for (const node of childNodes) {
+		if (node === null) {
+			continue;
+		}
+		// comopnent
+		if (node instanceof Component) {
+			fragment.appendChild(node.getElement());
+			continue;
+		}
+
+		fragment.appendChild(node);
+	}
+	newElement.appendChild(fragment);
+
+	return newElement;
+};
+
+const sideTab = document.querySelector(".flex-shrink-0.col-3");
+
+const
+
+const title = createElement("div", {
+	class: "discussion-sidebar-heading text-bold",
+	text: "Review Makes Developer",
+});
+
+sideTab.prepend(title);
