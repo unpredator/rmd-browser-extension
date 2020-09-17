4e44e2db7867fc4b07172d7db55c23f35adae63a
andy nam <naamoonoo.y@gmail.com>
2020-09-17 19:50:20 +0900

    feat: onClickHanlder added at button(#5)

--- src/index.js
+++ src/index.js
@@ -1,61 +1,69 @@
 const createElement = (tagName, attributes, ...childNodes) => {
-	const newElement = document.createElement(tagName);
+  const newElement = document.createElement(tagName);
 
-	for (const [key, value] of Object.entries(attributes)) {
-		// text
-		if (key === "text") {
-			const textNode = document.createTextNode(value);
-			newElement.appendChild(textNode);
-			continue;
-		}
+  for (const [key, value] of Object.entries(attributes)) {
+    // text
+    if (key === 'text') {
+      const textNode = document.createTextNode(value);
+      newElement.appendChild(textNode);
+      continue;
+    }
 
-		// event
-		if (typeof value === "function") {
-			const eventName = key.slice(2);
-			if (!attributes.eventTarget) {
-				newElement.addEventListener(eventName, value);
-				continue;
-			}
-		}
+    // event
+    if (typeof value === 'function') {
+      const eventName = key.slice(2).toLowerCase();
+      if (!attributes.eventTarget) {
+        newElement.addEventListener(eventName, value);
+        continue;
+      }
+    }
 
-		newElement.setAttribute(key, value);
-	}
+    newElement.setAttribute(key, value);
+  }
 
-	// childNodes
-	const fragment = document.createDocumentFragment();
-	for (const node of childNodes) {
-		if (node === null) {
-			continue;
-		}
+  // childNodes
+  const fragment = document.createDocumentFragment();
+  for (const node of childNodes) {
+    if (node === null) {
+      continue;
+    }
 
-		fragment.appendChild(node);
-	}
-	newElement.appendChild(fragment);
+    fragment.appendChild(node);
+  }
+  newElement.appendChild(fragment);
 
-	return newElement;
+  return newElement;
+};
+
+const onClickHandler = (e) => {
+  e.preventDefault();
+
+  window.open('http://naver.com');
 };
 
 const sideTab =
-	document.querySelector(".flex-shrink-0.col-3") || // pull request
-	document.querySelector(".flex-shrink-0.col-12.col-md-3"); // create pull request
+  document.querySelector('.flex-shrink-0.col-3') || // pull request
+  document.querySelector('.flex-shrink-0.col-12.col-md-3'); // create pull request
 
-const title = createElement("div", {
-	class: "discussion-sidebar-heading text-bold",
-	text: "Review Makes Developer",
+console.log('pr case');
+const title = createElement('div', {
+  class: 'discussion-sidebar-heading text-bold',
+  text: 'Review Makes Developer',
 });
 
 const form = createElement(
-	"form",
-	{ class: "thread-subscribe-form" },
-	createElement("button", {
-		class: "btn btn-block btn-sm thread-subscribe-button black-and-white",
-		text: "request review with RMD",
-	}),
-	createElement("p", {
-		class: "reason text-small text-gray",
-		text:
-			"unpredator encourage every developers to take one step further with reviewing each others code",
-	})
+  'form',
+  { class: 'thread-subscribe-form' },
+  createElement('button', {
+    class: 'btn btn-block btn-sm thread-subscribe-button black-and-white',
+    text: 'request review with RMD',
+    onClick: onClickHandler,
+  }),
+  createElement('p', {
+    class: 'reason text-small text-gray',
+    text:
+      'unpredator encourage every developers to take one step further with reviewing each others code',
+  })
 );
 
 sideTab.prepend(form);
